5503cab266dff36fa2bb8ed2f602e08c2076c118
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-08-22 22:22:20 +0900

    indexのごちゃごちゃ減らす、model追加

--- app/models.py
+++ app/models.py
@@ -7,7 +7,17 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField("タイトル", max_length=200)
     content = models.TextField("本文")
-    createed = models.DateTimeField("作成日", default=timezone.now)
+    address = models.TextField("住所")
+    latitude = models.FloatField("緯度")
+    longitude = models.FloatField("経度")
+    problemCategory = models.TextField("カテゴリー")
+    peopleNum = models.IntegerField("募集人員")
+    purpose = models.TextField("目的")
+    organization = models.TextField("搭載者種別")
+    problemSize = models.TextField("規模")
+    status = models.TextField("募集状態")
+    created = models.DateTimeField("作成日", default=timezone.now)
+
 
     def __str__(self):
         return self.title
