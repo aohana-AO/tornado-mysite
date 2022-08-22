c5407a7d2d2139026aa5841d00aacce1dc530993
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-08-23 01:41:41 +0900

    投稿の削除機能作成

--- app/views.py
+++ app/views.py
@@ -81,3 +81,12 @@ class PostEditView(LoginRequiredMixin,View):
         return render(request, 'app/post_form.html', {
             'form': form
         })
+class PostDeleteView(View):
+    def get(self,request,*args, **kwargs):
+        post_data=Post.objects.get(id=self.kwargs['pk'])
+        return render(request,'app/post_delete.html',{'post_data':post_data})
+
+    def post(self,request,*args, **kwargs):
+        post_data=Post.objects.get(id=self.kwargs['pk'])
+        post_data.delete()
+        return redirect('index')
