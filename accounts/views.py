1bc20c712f186968f114073a541c1be6024315fa
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-08-23 22:10:35 +0900

    logout追加

--- accounts/views.py
+++ accounts/views.py
@@ -22,3 +22,6 @@ class LogoutView(views.LogoutView):
         if self.request.user.is_authenticated:
             self.logout()
         return redirect('/')
+class SignupView(views.SignupView):
+    template_name='accounts/signup.html'
+    #from_class=SignupUserForm
