8e794988395968488ee17a38ed9b446b9961dfe2
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2022-08-23 14:54:04 +0900

    全てのform完成（動作不良あり）

--- app/forms.py
+++ app/forms.py
@@ -2,5 +2,77 @@ from django import forms
 
 
 class PostForm(forms.Form):
-    title=forms.CharField(max_length=30,label='タイトル',widget=forms.Textarea(attrs={'cols': '180', 'rows': '1'}))
-    content=forms.CharField(label='内容',widget=forms.Textarea(attrs={'cols': '180', 'rows': '10'}))
+    title = forms.CharField(max_length=30, label='タイトル', widget=forms.Textarea(attrs={'cols': '100', 'rows': '2'}))
+    content = forms.CharField(label='内容', widget=forms.Textarea(attrs={'cols': '140', 'rows': '6'}))
+
+    address = forms.CharField(label='住所', widget=forms.Textarea(attrs={'cols': '100', 'rows': '1'}))
+    latitude = forms.FloatField(label='緯度', widget=forms.Textarea(attrs={'cols': '100', 'rows': '1'}))
+    longitude = forms.FloatField(label='内容', widget=forms.Textarea(attrs={'cols': '100', 'rows': '1'}))
+
+    problemCategory = forms.fields.ChoiceField(
+
+        choices=(
+            ('環境問題', '環境問題'),
+            ('人間関係', '人間関係'),
+            ('貧困', '貧困'),
+            ('資源問題', '資源問題'),
+            ('その他', 'その他')
+        ),
+        label='カテゴリー',
+        required=True,
+        widget=forms.widgets.Select
+    )
+    peopleNum = forms.fields.ImageField(
+
+        label='内容', widget=forms.Textarea(attrs={'cols': '100', 'rows': '1'})
+    )
+    purpose = forms.fields.ChoiceField(
+
+        choices=(
+            ('議論したい', '議論したい'),
+            ('相談したい', '相談したい'),
+            ('行動したい', '行動したい'),
+            ('その他', 'その他'),
+
+        ),
+        label='目的',
+        required=True,
+        widget=forms.widgets.Select
+    )
+    organization = forms.fields.ChoiceField(
+
+        choices=(
+            ('個人', '個人'),
+            ('NPO・公共団体', 'NPO・公共団体'),
+            ('地方自治体', '地方自治体'),
+            ('一般企業', '一般企業'),
+            ('その他', 'その他')
+        ),
+        label='搭載者種別',
+        required=True,
+        widget=forms.widgets.Select
+    )
+    problemSize = forms.fields.ChoiceField(
+
+        choices=(
+            ('小', '小'),
+            ('中', '中'),
+            ('大', '大'),
+            ('その他', 'その他'),
+        ),
+        label='規模',
+        required=True,
+        widget=forms.widgets.Select
+    )
+    status = forms.fields.ChoiceField(
+
+        choices=(
+            ('募集中', '募集中'),
+            ('募集終了（断念', '募集終了（断念）'),
+            ('募集終了（解決）', '募集終了（解決）'),
+
+        ),
+        label='募集状態',
+        required=True,
+        widget=forms.widgets.Select
+    )
--- app/views.py
+++ app/views.py
@@ -29,6 +29,15 @@ class CreatePostView(LoginRequiredMixin,View):
             post_data.author = request.user
             post_data.title = form.cleaned_data['title']
             post_data.content = form.cleaned_data['content']
+            post_data.address = form.cleaned_data['address']
+            post_data.latitude = form.cleaned_data['latitude']
+            post_data.longitude = form.cleaned_data['longitude']
+            post_data.problemCategory = form.cleaned_data['problemCategory']
+            post_data.peopleNum = form.cleaned_data['peopleNum']
+            post_data.purpose = form.cleaned_data['purpose']
+            post_data.status = form.cleaned_data['status ']
+            post_data.problemSize = form.cleaned_data['problemSize']
+            post_data.organization = form.cleaned_data['organization']
 
             if request.FILES:
                 post_data.image = request.FILES.get('image')
@@ -48,6 +57,9 @@ class PostEditView(LoginRequiredMixin,View):
                 'title': post_data.title,
                 'content': post_data.content,
                 'author': post_data.author,
+                'address': post_data.address,
+                'latitude': post_data.latitude,
+                'longitude': post_data.longitude,
                 'status': post_data.status,
                 'organization': post_data.organization,
                 'peopleNum': post_data.peopleNum,
@@ -59,7 +71,7 @@ class PostEditView(LoginRequiredMixin,View):
 
 
         )
-        return  render(request, 'app/post_form.html',{
+        return render(request, 'app/post_form.html',{
             'form': form
         })
 
@@ -72,6 +84,8 @@ class PostEditView(LoginRequiredMixin,View):
             post_data.author = request.user
             post_data.title = form.cleaned_data['title']
             post_data.content = form.cleaned_data['content']
+            post_data.address = form.cleaned_data['address']
+            post_data.latitude = form.cleaned_data['latitude']
 
             if request.FILES:
                 post_data.image = request.FILES.get('image')
